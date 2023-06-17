7c42982770c67ee2ecb63d6b29c02f0765892f2a
isotheafcs <ismailsimsekoglu@gmail.com>
2023-06-17 13:37:18 +0300

    Source code changed

--- src/main/java/sm703/HomeController.java
+++ src/main/java/sm703/HomeController.java
@@ -1,14 +1,22 @@
 package sm703;
+
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 @Controller
 public class HomeController {
 
-    @Get
-    public Map<String, Object> index() {
-        return Collections.singletonMap("message", "Hello World");
+    @Get("/add/{a}/{b}")
+    public Map<String, Object> addNumbers(int a, int b) {
+        int sum = a + b;
+        Map<String, Object> response = new HashMap<>();
+        response.put("a", a);
+        response.put("b", b);
+        response.put("sum", sum);
+        return response;
     }
 }
